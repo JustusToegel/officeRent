4cc89282e872df61d81a05b42b3cb6d89b88e744
justus toegel <justus.toegel@gmx.de>
2024-02-28 16:55:38 +0100

    added validations

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -2,4 +2,8 @@ class Flat < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_one_attached :photo
+
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :address, presence: true
 end
